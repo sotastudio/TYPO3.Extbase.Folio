49b713c4b46b110f3e041d13fed8849284ff5a00
Andy Hausmann <andy.hausmann@gmx.de>
2012-04-28 08:57:18 +0000

    Did a small Cleanup.

--- ext_tables.php
+++ ext_tables.php
@@ -11,87 +11,87 @@ Tx_Extbase_Utility_Extension::registerPlugin(
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Folio');
 
-			t3lib_extMgm::addLLrefForTCAdescr('tx_folio_domain_model_customer', 'EXT:folio/Resources/Private/Language/locallang_csh_tx_folio_domain_model_customer.xml');
-			t3lib_extMgm::allowTableOnStandardPages('tx_folio_domain_model_customer');
-			$TCA['tx_folio_domain_model_customer'] = array(
-				'ctrl' => array(
-					'title'	=> 'LLL:EXT:folio/Resources/Private/Language/locallang_db.xml:tx_folio_domain_model_customer',
-					'label' => 'name',
-					'tstamp' => 'tstamp',
-					'crdate' => 'crdate',
-					'cruser_id' => 'cruser_id',
-					'dividers2tabs' => TRUE,
-					'versioningWS' => 2,
-					'versioning_followPages' => TRUE,
-					'sortby' => 'sorting',
-					'origUid' => 't3_origuid',
-					'languageField' => 'sys_language_uid',
-					'transOrigPointerField' => 'l10n_parent',
-					'transOrigDiffSourceField' => 'l10n_diffsource',
-					'delete' => 'deleted',
-					'enablecolumns' => array(
-						'disabled' => 'hidden',
-						'starttime' => 'starttime',
-						'endtime' => 'endtime',
-					),
-					'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Customer.php',
-					'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_folio_domain_model_customer.gif'
-				),
-			);
+t3lib_extMgm::addLLrefForTCAdescr('tx_folio_domain_model_customer', 'EXT:folio/Resources/Private/Language/locallang_csh_tx_folio_domain_model_customer.xml');
+t3lib_extMgm::allowTableOnStandardPages('tx_folio_domain_model_customer');
+$TCA['tx_folio_domain_model_customer'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:folio/Resources/Private/Language/locallang_db.xml:tx_folio_domain_model_customer',
+		'label' => 'name',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+		'sortby' => 'sorting',
+		'origUid' => 't3_origuid',
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Customer.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_folio_domain_model_customer.gif'
+	),
+);
 
-			t3lib_extMgm::addLLrefForTCAdescr('tx_folio_domain_model_project', 'EXT:folio/Resources/Private/Language/locallang_csh_tx_folio_domain_model_project.xml');
-			t3lib_extMgm::allowTableOnStandardPages('tx_folio_domain_model_project');
-			$TCA['tx_folio_domain_model_project'] = array(
-				'ctrl' => array(
-					'title'	=> 'LLL:EXT:folio/Resources/Private/Language/locallang_db.xml:tx_folio_domain_model_project',
-					'label' => 'name',
-					'tstamp' => 'tstamp',
-					'crdate' => 'crdate',
-					'cruser_id' => 'cruser_id',
-					'dividers2tabs' => TRUE,
-					'versioningWS' => 2,
-					'versioning_followPages' => TRUE,
-					'origUid' => 't3_origuid',
-					'languageField' => 'sys_language_uid',
-					'transOrigPointerField' => 'l10n_parent',
-					'transOrigDiffSourceField' => 'l10n_diffsource',
-					'delete' => 'deleted',
-					'enablecolumns' => array(
-						'disabled' => 'hidden',
-						'starttime' => 'starttime',
-						'endtime' => 'endtime',
-					),
-					'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Project.php',
-					'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_folio_domain_model_project.gif'
-				),
-			);
+t3lib_extMgm::addLLrefForTCAdescr('tx_folio_domain_model_project', 'EXT:folio/Resources/Private/Language/locallang_csh_tx_folio_domain_model_project.xml');
+t3lib_extMgm::allowTableOnStandardPages('tx_folio_domain_model_project');
+$TCA['tx_folio_domain_model_project'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:folio/Resources/Private/Language/locallang_db.xml:tx_folio_domain_model_project',
+		'label' => 'name',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+		'origUid' => 't3_origuid',
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Project.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_folio_domain_model_project.gif'
+	),
+);
 
-			t3lib_extMgm::addLLrefForTCAdescr('tx_folio_domain_model_tags', 'EXT:folio/Resources/Private/Language/locallang_csh_tx_folio_domain_model_tags.xml');
-			t3lib_extMgm::allowTableOnStandardPages('tx_folio_domain_model_tags');
-			$TCA['tx_folio_domain_model_tags'] = array(
-				'ctrl' => array(
-					'title'	=> 'LLL:EXT:folio/Resources/Private/Language/locallang_db.xml:tx_folio_domain_model_tags',
-					'label' => 'uid',
-					'tstamp' => 'tstamp',
-					'crdate' => 'crdate',
-					'cruser_id' => 'cruser_id',
-					'dividers2tabs' => TRUE,
-					'versioningWS' => 2,
-					'versioning_followPages' => TRUE,
-					'origUid' => 't3_origuid',
-					'languageField' => 'sys_language_uid',
-					'transOrigPointerField' => 'l10n_parent',
-					'transOrigDiffSourceField' => 'l10n_diffsource',
-					'delete' => 'deleted',
-					'enablecolumns' => array(
-						'disabled' => 'hidden',
-						'starttime' => 'starttime',
-						'endtime' => 'endtime',
-					),
-					'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Tags.php',
-					'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_folio_domain_model_tags.gif'
-				),
-			);
+t3lib_extMgm::addLLrefForTCAdescr('tx_folio_domain_model_tags', 'EXT:folio/Resources/Private/Language/locallang_csh_tx_folio_domain_model_tags.xml');
+t3lib_extMgm::allowTableOnStandardPages('tx_folio_domain_model_tags');
+$TCA['tx_folio_domain_model_tags'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:folio/Resources/Private/Language/locallang_db.xml:tx_folio_domain_model_tags',
+		'label' => 'uid',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+		'origUid' => 't3_origuid',
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Tags.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_folio_domain_model_tags.gif'
+	),
+);
 
 $tmp_folio_columns = array(
 
